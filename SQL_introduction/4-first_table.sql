4bf553c4cad13db1e1f72686ff4c708a58b9fa90
Christian-Regnante <c.ngwino@alustudent.com>
2025-03-14 11:03:05 -0700

    SQL Introduction

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+--  a script that creates a table called first_table in the current database --
+CREATE TABLE IF NOT EXISTS `first_table`(`id` INT(11) DEFAULT NULL, `name` VARCHAR(256) DEFAULT NULL);
